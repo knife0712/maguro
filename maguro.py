0c780e3d34d6cf175c6669bcc7be53f105dd2104
SatoYuta <xy0123.y224@gmail.com>
2021-05-08 18:02:36 +0900

    [change] オンライン/オフラインの通知をするチャンネルを変更

--- maguro.py
+++ maguro.py
@@ -4,6 +4,7 @@ import datetime # 時刻に関する機能
 import discord # ディスコードの基本機能
 
 TEXT_CHANNEL_ID = 839366320691609643
+MAGURO_CHANNEL_ID = 840513454497595392
 
 # Botのトークンを取得
 BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
@@ -42,7 +43,7 @@ async def on_member_update(before, after):
     # ステータスの変化を日本語にするための変換表
     status_table = {"online":"オンライン" ,"idle":"退席中", "dnd":"取り込み中", "offline":"オフライン"}
     # メッセージを送信するチャンネル
-    channel = client.get_channel(TEXT_CHANNEL_ID)
+    channel = client.get_channel(MAGURO_CHANNEL_ID)
     # 現在時刻（サーバーの関係で9時間ずれているので補正）
     current_datetime = datetime.datetime.now()+datetime.timedelta(hours=9)
 
