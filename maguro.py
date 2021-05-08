e2d11f7fc6adde75754dde9693caac821b016d3e
SatoYuta <xy0123.y224@gmail.com>
2021-05-08 16:04:25 +0900

    [feat] 名前を呼ばれたときに反応するようにした

--- maguro.py
+++ maguro.py
@@ -12,14 +12,22 @@ client = discord.Client()
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    # 自分自身のメッセージに反応しないようにするため
+    if message.author.bot:
+        return
+
     # 寿司の絵文字が送られてきたとき
     if "\N{SUSHI}" in message.content:
         # :yum: の絵文字を返す
         await message.channel.send("\N{FACE SAVOURING DELICIOUS FOOD}")
-        
-    # 受け取ったメッセージが自分に対するメンションだった場合
-    #if client.user.mentioned_in(message):
 
+    # 受け取ったメッセージが自分に対するメンションだった場合
+    if client.user.mentioned_in(message):
+        # "マグロちゃん" がメッセージ中に入っていた場合
+        if "マグロちゃん" in message.content:
+            # 寿司を提供する
+            await message.channel.send("へいおまちっ！" + "\N{SUSHI}")
 
 # Botの起動とDiscordサーバーへの接続
 client.run(BOT_TOKEN)
