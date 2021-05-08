08530d01bd2fd45c8d17e9c0bd8f1d395ce56f13
SatoYuta <xy0123.y224@gmail.com>
2021-05-08 15:52:23 +0900

    [feat] 寿司の絵文字に反応するようにした

--- maguro.py
+++ maguro.py
@@ -9,5 +9,17 @@ BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 # これが無いとサーバーにアクセスできない
 client = discord.Client()
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # 寿司の絵文字が送られてきたとき
+    if "\N{SUSHI}" in message.content:
+        # :yum: の絵文字を返す
+        await message.channel.send("\N{FACE SAVOURING DELICIOUS FOOD}")
+        
+    # 受け取ったメッセージが自分に対するメンションだった場合
+    #if client.user.mentioned_in(message):
+
+
 # Botの起動とDiscordサーバーへの接続
 client.run(BOT_TOKEN)
